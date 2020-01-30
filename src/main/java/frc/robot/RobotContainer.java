81241e36ece1f55b3047f4376f9e242295a8f235
Julian <jeavila2@cps.edu>
2020-01-30 16:24:55 -0600

    finally made subsystem but not complete

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ColorSensor;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.Motor;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -24,7 +25,7 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   public final Motor motorSub = new Motor();
-
+  public final ColorSensor colorSense = new ColorSensor();
   
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
