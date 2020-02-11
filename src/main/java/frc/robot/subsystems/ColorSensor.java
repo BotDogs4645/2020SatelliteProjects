95ac675706bcc874e73407cc458c4dfe5ccbf302
frc <frc@I46221L66550570.instr.cps.k12.il.us>
2020-02-11 16:24:57 -0600

    color sensor

--- src/main/java/frc/robot/subsystems/ColorSensor.java
+++ src/main/java/frc/robot/subsystems/ColorSensor.java
@@ -6,15 +6,48 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
+//import frc.robot.commands.DetectColor;
+//import frc.robot.commands.MoveByColor;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 public class ColorSensor extends SubsystemBase {
+  I2C colorSensor = new I2C(I2C.Port.kOnboard, 0x1E);
+	AnalogInput csAnalog = new AnalogInput(0);
+	public void setActive() {
+		colorSensor.write(0x03, 0x00); 
+  }
+  
+  public int[] getColor(){
+    byte [] blue = new byte[1];
+    byte[] colorValue = new byte[1];
+
+    colorSensor.read(0x07, 1, blue);
+    colorSensor.read(0x04, 1, colorValue);
+
+    SmartDashboard.putNumber("Color: ", colorValue[0]);
+
+    SmartDashboard.putNumber("Blue Value: ", blue[0]);
+
+    int[] colorReturn = {blue[0], colorValue[0]};
+
+    return colorReturn;
+  }
+
+  public void setPassive() {
+		colorSensor.write(0x03, 0x01); 
+	}
+
+
   /**
    * Creates a new ColorSensor.
    */
   public ColorSensor() {
+    
 
   }
 
